8866a51af1c50e79ad899fb6a7874fc077acfb35
vanilla-terrykerrigan <terrykerrigan428@gmail.com>
2020-11-29 12:57:55 +0900

    입출금 내역 분석시 1차 요구사항에 대한 기능 구현 및 리팩토링 완료

---
+++ happy_java_study/src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,13 @@
+import bank_statements_analyzer.BankStatementCSVParser;
+import bank_statements_analyzer.BankStatementParse;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+    private final BankStatementParse bankStatementParse = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("not yet implemented");
+    }
+}
