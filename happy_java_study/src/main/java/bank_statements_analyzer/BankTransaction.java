46daf1d00ba6d49cb421d11c69c6b40bd3077083
vanilla-terrykerrigan <terrykerrigan428@gmail.com>
2020-11-29 10:23:35 +0900

    class which conversion to java object from string transaction data

--- happy_java_study/src/main/java/bank_statements_analyzer/BankTransaction.java
+++ happy_java_study/src/main/java/bank_statements_analyzer/BankTransaction.java
@@ -1,2 +1,36 @@
-package bank_statements_analyzer;public class BankTransaction {
+package bank_statements_analyzer;
+
+import java.time.LocalDate;
+
+public class BankTransaction {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(final LocalDate date, final double amount, final String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    //get date
+    public LocalDate getDate() {
+        return date;
+    }
+
+    //get amount
+    public double getAmount() {
+        return amount;
+    }
+
+    //get description
+    public String getDescription() {
+        return description;
+    }
+
+    //make deposit info to string
+    @Override
+    public String toString() {
+        return " date="+date+" amount="+amount+" description="+description;
+    }
 }
