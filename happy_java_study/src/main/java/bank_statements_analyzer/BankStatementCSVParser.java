a558f189685265e301d3e2f90db3fe3b991ad40e
vanilla-terrykerrigan <terrykerrigan428@gmail.com>
2020-11-29 10:24:22 +0900

    parsing logic class

--- happy_java_study/src/main/java/bank_statements_analyzer/BankStatementCSVParser.java
+++ happy_java_study/src/main/java/bank_statements_analyzer/BankStatementCSVParser.java
@@ -1,2 +1,32 @@
-package bank_statements_analyzer;public class BankStatementCSVParser {
+package bank_statements_analyzer;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    // conversion to BankTransaction from string line
+    public BankTransaction parseFromCSV(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final Double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    // read all string lines of bank transaction data and conversion to BankTransaction list object from String list.
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+
+        final List<BankTransaction> bankTransactions = new ArrayList<BankTransaction>();
+        for(final String line: lines) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
+    }
 }
