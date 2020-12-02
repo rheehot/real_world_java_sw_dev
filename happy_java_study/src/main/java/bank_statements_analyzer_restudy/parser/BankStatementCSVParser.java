bb05e969bed1e80a7a810127d3a7d3dad43ebf97
vanilla-terrykerrigan <terrykerrigan428@gmail.com>
2020-12-02 17:14:28 +0900

    parser 1차 기능 구현

--- happy_java_study/src/main/java/bank_statements_analyzer_restudy/parser/BankStatementCSVParser.java
+++ happy_java_study/src/main/java/bank_statements_analyzer_restudy/parser/BankStatementCSVParser.java
@@ -9,5 +9,25 @@ import java.util.List;
 
 public class BankStatementCSVParser implements BankStatementParser{
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public List<BankTransaction> parseLinesFrom(final List<String> lines) {
+        List<BankTransaction> bankTransactions = new ArrayList<BankTransaction>();
 
+        for(String line: lines) {
+
+            final String[] columns = line.split(",");
+
+            // to date
+            final LocalDate date = LocalDate.parse(columns[0], DATE_TIME_FORMATTER);
+
+            // to double
+            final double amount = Double.parseDouble(columns[1]);
+
+            BankTransaction bankTransaction = new BankTransaction(date, amount, columns[2]);
+
+            bankTransactions.add(bankTransaction);
+        }
+
+        return bankTransactions;
+    }
 }
--- happy_java_study/src/main/java/bank_statements_analyzer_restudy/parser/BankStatementParser.java
+++ happy_java_study/src/main/java/bank_statements_analyzer_restudy/parser/BankStatementParser.java
@@ -5,5 +5,5 @@ import bank_statements_analyzer_restudy.dao.BankTransaction;
 import java.util.List;
 
 public interface BankStatementParser {
-
+    List<BankTransaction> parseLinesFrom(List<String> lines);
 }
